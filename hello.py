59ff0403c162a9ae3440805a024d005ac77c7773
Camila <161645920+CamilaNR@users.noreply.github.com>
2025-05-11 15:09:26 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Welcome!")
+name= input ("name:")
+print("Hello," + name)
+city= input("enter your city:")
+print("Welcome " + name + " from " + city)
+n= int( input ("Number: "))
+if n > 3:
+    print("n is positive")
+elif n < 3:
+    print("n is negative")
+else:
+    print("n is zero") 
+print("How can I help you," + name + "?")
+request= input ("Tell me what you need:")
+print("You said:"+ request) 
+print("I don't know, try it Goole or ChatGBT. Good Luck!")
+
+
